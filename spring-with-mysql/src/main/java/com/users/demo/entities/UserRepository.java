9ceae334b892968c83b52fd9fda2b70f4f98c4e1
ummerumana <umme.rumana@razorthink.com>
2020-02-26 17:34:56 +0530

    spring new example

--- spring-with-mysql/src/main/java/com/users/demo/entities/UserRepository.java
+++ spring-with-mysql/src/main/java/com/users/demo/entities/UserRepository.java
@@ -1,10 +1,12 @@
 package com.users.demo.entities;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+@Repository
 public interface UserRepository extends CrudRepository<Customer, Integer> {
 
 }
