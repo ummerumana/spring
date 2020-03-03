17c49a96f7664bce97d90de60d8c0860e1101f99
ummerumana <umme.rumana@razorthink.com>
2020-03-03 14:36:02 +0530

    springboot with mysql and docker

---
+++ spring-mysql-docker/src/main/java/com/users/demo/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.users.demo.controller;
+
+import com.users.demo.entities.Customer;
+import com.users.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/customer")
+
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/all")
+    List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+   @RequestMapping(method = RequestMethod.GET, value = "/get/{id}")
+    Customer getCustomerById(@PathVariable("id") int id)
+    {
+        return customerService.getCustomerById(id);
+    }
+
+   @RequestMapping(method = RequestMethod.DELETE, value = "/remove/{id}" )
+    void deleteCustomerById(@PathVariable("id") int id)
+    {
+        customerService.deleteCustomerById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/update/{id}")
+    void updateCustomer(@PathVariable("id") int id,@RequestBody Customer customer)
+    {
+        customerService.updateCustomerById(id, customer);
+    }
+
+     @RequestMapping(method = RequestMethod.POST,value = "/insert")
+    void insertCustomer(@RequestBody Customer customer){
+        customerService.insertCustomer(customer);
+    }
+}
---
+++ spring-mysql-docker/src/main/java/com/users/demo/entities/Customer.java
@@ -0,0 +1,93 @@
+package com.users.demo.entities;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Component
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CustomerId")
+    private int id;
+    @Column(name = "CustomerFname")
+    private String firstName;
+    @Column(name = "CustomerLname")
+    private String lastName;
+    @Column(name = "CustomerEmail")
+    private String email;
+    @Column(name = "CustomerAddress")
+    private String address;
+    @Column(name = "CustomerPhone")
+    private long phone;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email, String address, long phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
